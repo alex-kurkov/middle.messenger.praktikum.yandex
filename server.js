005469f644e23ad1c09137bd87668c61f62a1961
alex-kurkov <alexkourkov@yandex.ru>
2022-12-07 15:17:43 +0500

    enrolled all content into main

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+
+const PORT = process.env.NODE_PORT || 3000;
 
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
-app.listen(PORT = 3000, () => {
-  console.log(`\nсервер доступен по адресу http://localhost:${PORT}\n`)
+app.listen(PORT, () => {
+  console.log(`\nсервер доступен по адресу http://localhost:${PORT}\n`);
 });
